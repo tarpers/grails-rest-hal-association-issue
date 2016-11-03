8e671ae926b2be62756764df349c3677161b348c
Simon Tarplin <simon.tarplin@gamesys.co.uk>
2016-11-03 08:42:36 +0000

    Initial commit of plain Grails project using the REST profile.

---
+++ grails-app/controllers/grails/rest/hal/association/issue/ApplicationController.groovy
@@ -0,0 +1,15 @@
+package grails.rest.hal.association.issue
+
+import grails.core.GrailsApplication
+import grails.util.Environment
+import grails.plugins.*
+
+class ApplicationController implements PluginManagerAware {
+
+    GrailsApplication grailsApplication
+    GrailsPluginManager pluginManager
+
+    def index() {
+        [grailsApplication: grailsApplication, pluginManager: pluginManager]
+    }
+}
---
+++ grails-app/init/grails/rest/hal/association/issue/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails.rest.hal.association.issue
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
